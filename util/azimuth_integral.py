e7f3a1f55427b46f1b1e606f8d2db699d8ccadb8
董成栋 <48635985+DongChengdongHangZhou@users.noreply.github.com>
2021-10-10 14:22:21 +0800

    Update azimuth_integral.py

--- util/azimuth_integral.py
+++ util/azimuth_integral.py
@@ -4,6 +4,7 @@ import torch
 
 def GetPSD1D(psd2D):
     psd2D = 0.5*psd2D + 0.5
+    psd2D[:,:,128,128] = 1   # we must ensure the center of the spectrum is 1, to stablize the training.
     batch_size = psd2D.shape[0]
     psd2D = torch.exp(psd2D*16)-1
     Y, X = np.ogrid[0:256, 0:256]
@@ -17,20 +18,14 @@ def GetPSD1D(psd2D):
     psd1D = (result.t()/result.t()[0]).t().cuda(0) # every element is devided by the value of the first column. result[i][j]/result[i][0]
     return torch.reshape(psd1D,(batch_size,2,8,8))      
 
-def crossEntropyFourierAzimuth(tensor1,tensor2):
-
-    '''
-    you must rewrite your own crossEntropyLoss since
-    the pytorch version of crossEntropyLoss is
-    (-p(x)*log(q(x))).sum()
-    but the crossEntropyLoss applied in this paper is
-    (-p(x)*log(q(x))-(1-p(x))*log(1-q(x))).sum()
-    for the crossEntropyLoss, the sequence of tensor1
-    and tensor2 cannot be changed
-    '''
-    loss = torch.relu(((tensor1-tensor2)*(tensor1-tensor2)).sum()-0.25)
+def Azimuthloss(tensor1,tensor2):
+    loss = torch.relu(((tensor1-tensor2)*(tensor1-tensor2)).sum())*200
     return loss        
 
-def powerloss(tensor,upper,bottom):
-    loss = ((tensor-upper)*torch.relu(tensor-upper)).sum()+((bottom-tensor)*torch.relu(bottom-tensor)).sum()
+def powerloss(tensor1,tensor2):
+    loss = ((tensor1-tensor2)*(tensor1-tensor2)).sum()
+    return loss
+
+def spectralloss(tensor_real,tensor_fake):
+    loss =(-1/128) * ((tensor_real*torch.log(tensor_fake)).sum() + ((1.01-tensor_real)*torch.log(1.01-tensor_fake)).sum())
     return loss
