ae93530bdbdcd273533785f895dbdcada967c36a
董成栋 <48635985+DongChengdongHangZhou@users.noreply.github.com>
2021-11-19 18:11:57 +0800

    Update val_cyclegan.sh

--- scripts/val_cyclegan.sh
+++ scripts/val_cyclegan.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=2 python val.py --dataroot ./datasets/fingerprint --name fingerprint --save_name fingerprint --phase test --no_dropout
+CUDA_VISIBLE_DEVICES=1 python val.py --dataroot ./datasets/cyclegan --name cycleganA --save_name cycleganA --phase test --no_dropout
 # --dataroot: the directory of testing dataset. --name: the checkpoints directory. --save_name: save the generated spectrum to this directory.
